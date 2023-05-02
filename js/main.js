4c362281ebe64edeb07a4f74ed52bb3c0aba94fb
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-05-02 18:40:19 +0200

    update: exp session

--- js/main.js
+++ js/main.js
@@ -112,3 +112,109 @@ for (let i = 0; i < works.length; i += 1) {
     let workList = displayWorks(works[i]);
     workSection.innerHTML += workList;
 }
+
+const skillsExps = [
+    {
+        img: "assets/html.png",
+        title: "HTML 5",
+    },
+    {
+        img: "assets/sass.png",
+        title: "Sass",
+    },
+    {
+        img: "assets/typescript.png",
+        title: "Typescript",
+    },
+    {
+        img: "assets/python.png",
+        title: "Python",
+    },
+    {
+        img: "assets/redux.png",
+        title: "Redux",
+    },
+    {
+        img: "assets/node.png",
+        title: "Node JS",
+    },
+    {
+        img: "assets/vue.png",
+        title: "Vue",
+    },
+    {
+        img: "assets/figma.png",
+        title: "Figma",
+    },
+    {
+        img: "assets/css.png",
+        title: "CSS3",
+    },
+    {
+        img: "assets/react.png",
+        title: "React JS",
+    },
+    {
+        img: "assets/flutter.png",
+        title: "Flutter",
+    },
+    {
+        img: "assets/mu5.png",
+        title: "Material UI",
+    },
+];
+
+const exps = [
+    {
+        date: "2021",
+        title: "Personal Portfolio",
+        comp: "Web App",
+    },
+    {
+        date: "2023",
+        title: "Personal Portfolio",
+        comp: "Web App",
+    },
+    {
+        date: "2022",
+        title: "Personal Portfolio",
+        comp: "Web App",
+    },
+];
+
+function displaySkillExp(skill){
+    const { img, title } = skill;
+    let skillList = `
+    <li>
+        <img src="${img}"/>
+        <p>${title}</p>
+    </>
+    `;
+    return skillList;
+}
+
+function displaySkillDesc(skill){
+    const { date, title, comp } = skill;
+    let desc = `
+    <li>
+        <h3>${date}</h3>
+        <ul>
+            <li>${title}</li>
+            <li>${comp}</li>
+        <ul>
+    </>
+    `;
+    return desc;
+}
+
+const skillExp = document.querySelector('.exp-imgs');
+const desc = document.querySelector('.exp-desc');
+
+for (let i = 0; i < skillsExps.length; i += 1) {
+    let skillList = displaySkillExp(skillsExps[i]);
+    skillExp.innerHTML += skillList;
+}
+for (let i = 0; i < exps.length; i += 1) {
+    let skillDesc = displaySkillDesc(exps[i]);
+    desc.innerHTML += skillDesc;
+}
