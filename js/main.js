3f24c93a936c7e2d1a2601c9f35eab77c8f44852
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-05-02 07:26:58 +0200

    update: work section

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ const skills = [
         paragraph: "",
         img: "assets/about04.png",
     },
-
 ];
 
 function displaySkill(skills) {
@@ -37,6 +36,79 @@ function displaySkill(skills) {
 const container = document.querySelector('.about-skills');
 for (let i = 0; i < skills.length; i+=1) {
     const content = displaySkill(skills[i]);
-    console.log(content)
     container.innerHTML += content;
+}
+
+const works = [
+    {
+        img: "assets/about01.png",
+        title: "Personal Portfolio",
+        type: "Web App",
+        desc: "",
+    },
+    {
+        img: "assets/about02.png",
+        title: "Capstone Project",
+        type: "Web App",
+        desc: "",
+    },
+    {
+        img: "assets/about03.png",
+        title: "Awesome Books",
+        type: "SPA",
+        desc: "",
+    },
+    {
+        img: "assets/about04.png",
+        title: "Coming soon",
+        type: "SPA",
+        desc: "",
+    },
+    {
+        img: "assets/about01.png",
+        title: "Coming soon",
+        type: "SPA",
+        desc: "",
+    },
+];
+
+function displayWorks(work){
+    const { img, title, desc, type } = work;
+    let workList = `
+    <li>
+        <img src="${img}"/>
+        <span>${type}</span>
+        <h2>${title}</h2>
+        <p>${desc}</p>
+    </li>
+    `;
+    return workList;
+}
+
+function displayType(type){
+    let typeList = `
+    <li>${type}</>
+    `;
+    return typeList;
+}
+const typesList = [];
+
+for (let i = 0; i < works.length; i++) {
+    const { type } = works[i];
+    if (!typesList.includes(type)) {
+        typesList.push(type);
+    }
+}
+
+const workSection = document.querySelector('.works');
+const types = document.querySelector('.work-types');
+
+for (let i = 0; i < typesList.length; i += 1) {
+    let typeList = displayType(typesList[i])
+    types.innerHTML += typeList;
+}
+
+for (let i = 0; i < works.length; i += 1) {
+    let workList = displayWorks(works[i]);
+    workSection.innerHTML += workList;
 }
