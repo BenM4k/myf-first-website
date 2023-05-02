7ce22f47baffdf45be191f5f7653ccb0bc778641
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-05-02 21:05:37 +0200

    fix linters error

--- js/main.js
+++ js/main.js
@@ -1,80 +1,82 @@
 const skills = [
-    {
-        title: "Frontend Developer",
-        paragraph: "",
-        img: "assets/about01.png",
-    },
-    {
-        title: "Search Engine Optimization",
-        paragraph: "",
-        img: "assets/about02.png",
-    },
-    {
-        title: "Dynamic websites",
-        paragraph: "",
-        img: "assets/about03.png",
-    },
-    {
-        title: "Javascript",
-        paragraph: "",
-        img: "assets/about04.png",
-    },
+  {
+    title: 'Frontend Developer',
+    paragraph: '',
+    img: 'assets/about01.png',
+  },
+  {
+    title: 'Search Engine Optimization',
+    paragraph: '',
+    img: 'assets/about02.png',
+  },
+  {
+    title: 'Dynamic websites',
+    paragraph: '',
+    img: 'assets/about03.png',
+  },
+  {
+    title: 'Javascript',
+    paragraph: '',
+    img: 'assets/about04.png',
+  },
 ];
 
 function displaySkill(skills) {
-    const { title, paragraph, img } = skills;
-    let a = `
+  const { title, paragraph, img } = skills;
+  const a = `
     <li>
         <img src="${img}" alt="${title}"/>
         <h2>${title}</h2>
         <p>${paragraph}</p>
     </li>
     `;
-    return a;
-};
+  return a;
+}
 
 const container = document.querySelector('.about-skills');
-for (let i = 0; i < skills.length; i+=1) {
-    const content = displaySkill(skills[i]);
-    container.innerHTML += content;
+for (let i = 0; i < skills.length; i += 1) {
+  const content = displaySkill(skills[i]);
+  container.innerHTML += content;
 }
 
 const works = [
-    {
-        img: "assets/about01.png",
-        title: "Personal Portfolio",
-        type: "Web App",
-        desc: "",
-    },
-    {
-        img: "assets/about02.png",
-        title: "Capstone Project",
-        type: "Web App",
-        desc: "",
-    },
-    {
-        img: "assets/about03.png",
-        title: "Awesome Books",
-        type: "SPA",
-        desc: "",
-    },
-    {
-        img: "assets/about04.png",
-        title: "To-do List",
-        type: "SPA",
-        desc: "",
-    },
-    {
-        img: "assets/about01.png",
-        title: "Coming soon",
-        type: "SPA",
-        desc: "",
-    },
+  {
+    img: 'assets/about01.png',
+    title: 'Personal Portfolio',
+    type: 'Web App',
+    desc: '',
+  },
+  {
+    img: 'assets/about02.png',
+    title: 'Capstone Project',
+    type: 'Web App',
+    desc: '',
+  },
+  {
+    img: 'assets/about03.png',
+    title: 'Awesome Books',
+    type: 'SPA',
+    desc: '',
+  },
+  {
+    img: 'assets/about04.png',
+    title: 'To-do List',
+    type: 'SPA',
+    desc: '',
+  },
+  {
+    img: 'assets/about01.png',
+    title: 'Coming soon',
+    type: 'SPA',
+    desc: '',
+  },
 ];
 
-function displayWorks(work){
-    const { img, title, desc, type } = work;
-    let workList = `
+function displayWorks(work) {
+  const {
+    img, title, desc, type,
+  } = work;
+  const workList = `
     <li>
         <img src="${img}"/>
         <span>${type}</span>
@@ -82,120 +84,120 @@ function displayWorks(work){
         <p>${desc}</p>
     </li>
     `;
-    return workList;
+  return workList;
 }
 
-function displayType(type){
-    let typeList = `
+function displayType(type) {
+  const typeList = `
     <li>${type}</>
     `;
-    return typeList;
+  return typeList;
 }
 const typesList = ['All'];
 
-for (let i = 0; i < works.length; i++) {
-    const { type } = works[i];
-    if (!typesList.includes(type)) {
-        typesList.push(type);
-    }
+for (let i = 0; i < works.length; i += 1) {
+  const { type } = works[i];
+  if (!typesList.includes(type)) {
+    typesList.push(type);
+  }
 }
 
 const workSection = document.querySelector('.works');
 const types = document.querySelector('.work-types');
 
 for (let i = 0; i < typesList.length; i += 1) {
-    let typeList = displayType(typesList[i])
-    types.innerHTML += typeList;
+  const typeList = displayType(typesList[i]);
+  types.innerHTML += typeList;
 }
 
 for (let i = 0; i < works.length; i += 1) {
-    let workList = displayWorks(works[i]);
-    workSection.innerHTML += workList;
+  const workList = displayWorks(works[i]);
+  workSection.innerHTML += workList;
 }
 
 const skillsExps = [
-    {
-        img: "assets/html.png",
-        title: "HTML 5",
-    },
-    {
-        img: "assets/sass.png",
-        title: "Sass",
-    },
-    {
-        img: "assets/typescript.png",
-        title: "Typescript",
-    },
-    {
-        img: "assets/python.png",
-        title: "Python",
-    },
-    {
-        img: "assets/redux.png",
-        title: "Redux",
-    },
-    {
-        img: "assets/node.png",
-        title: "Node JS",
-    },
-    {
-        img: "assets/vue.png",
-        title: "Vue",
-    },
-    {
-        img: "assets/figma.png",
-        title: "Figma",
-    },
-    {
-        img: "assets/css.png",
-        title: "CSS3",
-    },
-    {
-        img: "assets/react.png",
-        title: "React JS",
-    },
-    {
-        img: "assets/flutter.png",
-        title: "Flutter",
-    },
-    {
-        img: "assets/mu5.png",
-        title: "Material UI",
-    },
+  {
+    img: 'assets/html.png',
+    title: 'HTML 5',
+  },
+  {
+    img: 'assets/sass.png',
+    title: 'Sass',
+  },
+  {
+    img: 'assets/typescript.png',
+    title: 'Typescript',
+  },
+  {
+    img: 'assets/python.png',
+    title: 'Python',
+  },
+  {
+    img: 'assets/redux.png',
+    title: 'Redux',
+  },
+  {
+    img: 'assets/node.png',
+    title: 'Node JS',
+  },
+  {
+    img: 'assets/vue.png',
+    title: 'Vue',
+  },
+  {
+    img: 'assets/figma.png',
+    title: 'Figma',
+  },
+  {
+    img: 'assets/css.png',
+    title: 'CSS3',
+  },
+  {
+    img: 'assets/react.png',
+    title: 'React JS',
+  },
+  {
+    img: 'assets/api.png',
+    title: 'Api',
+  },
+  {
+    img: 'assets/mu5.png',
+    title: 'Material UI',
+  },
 ];
 
 const exps = [
-    {
-        date: "2021",
-        title: "Personal Portfolio",
-        comp: "Web App",
-    },
-    {
-        date: "2023",
-        title: "Personal Portfolio",
-        comp: "Web App",
-    },
-    {
-        date: "2022",
-        title: "Personal Portfolio",
-        comp: "Web App",
-    },
+  {
+    date: '2021',
+    title: 'Personal Portfolio',
+    comp: 'Web App',
+  },
+  {
+    date: '2023',
+    title: 'Personal Portfolio',
+    comp: 'Web App',
+  },
+  {
+    date: '2022',
+    title: 'Personal Portfolio',
+    comp: 'Web App',
+  },
 ];
 
-function displaySkillExp(skill){
-    const { img, title } = skill;
-    let skillList = `
+function displaySkillExp(skill) {
+  const { img, title } = skill;
+  const skillList = `
     <li>
         <img src="${img}"/>
         <p>${title}</p>
     </>
     `;
-    return skillList;
+  return skillList;
 }
 
-function displaySkillDesc(skill){
-    const { date, title, comp } = skill;
-    let desc = `
+function displaySkillDesc(skill) {
+  const { date, title, comp } = skill;
+  const desc = `
     <li>
         <h3>${date}</h3>
         <ul>
@@ -204,19 +206,19 @@ function displaySkillDesc(skill){
         <ul>
     </>
     `;
-    return desc;
+  return desc;
 }
 
 const skillExp = document.querySelector('.exp-imgs');
 const desc = document.querySelector('.exp-desc');
 
 for (let i = 0; i < skillsExps.length; i += 1) {
-    let skillList = displaySkillExp(skillsExps[i]);
-    skillExp.innerHTML += skillList;
+  const skillList = displaySkillExp(skillsExps[i]);
+  skillExp.innerHTML += skillList;
 }
 for (let i = 0; i < exps.length; i += 1) {
-    let skillDesc = displaySkillDesc(exps[i]);
-    desc.innerHTML += skillDesc;
+  const skillDesc = displaySkillDesc(exps[i]);
+  desc.innerHTML += skillDesc;
 }
 
 // spa
@@ -233,71 +235,71 @@ const sectionSkills = document.querySelector('.exp');
 const sectionContact = document.querySelector('.contacts');
 
 navHome.addEventListener('click', () => {
-    navHome.classList.add('active');
-    navAbout.classList.remove('active');
-    navWork.classList.remove('active');
-    navSkills.classList.remove('active');
-    navContact.classList.remove('active');
+  navHome.classList.add('active');
+  navAbout.classList.remove('active');
+  navWork.classList.remove('active');
+  navSkills.classList.remove('active');
+  navContact.classList.remove('active');
 
-    sectionHome.style.display = 'block';
-    sectionAbout.style.display = 'none';
-    sectionWork.style.display = 'none';
-    sectionSkills.style.display = 'none';
-    sectionContact.style.display = 'none';
+  sectionHome.style.display = 'block';
+  sectionAbout.style.display = 'none';
+  sectionWork.style.display = 'none';
+  sectionSkills.style.display = 'none';
+  sectionContact.style.display = 'none';
 });
 
 navAbout.addEventListener('click', () => {
-    navHome.classList.remove('active');
-    navAbout.classList.add('active');
-    navWork.classList.remove('active');
-    navSkills.classList.remove('active');
-    navContact.classList.remove('active');
+  navHome.classList.remove('active');
+  navAbout.classList.add('active');
+  navWork.classList.remove('active');
+  navSkills.classList.remove('active');
+  navContact.classList.remove('active');
 
-    sectionHome.style.display = 'none';
-    sectionAbout.style.display = 'block';
-    sectionWork.style.display = 'none';
-    sectionSkills.style.display = 'none';
-    sectionContact.style.display = 'none';
+  sectionHome.style.display = 'none';
+  sectionAbout.style.display = 'block';
+  sectionWork.style.display = 'none';
+  sectionSkills.style.display = 'none';
+  sectionContact.style.display = 'none';
 });
 
 navWork.addEventListener('click', () => {
-    navHome.classList.remove('active');
-    navAbout.classList.remove('active');
-    navWork.classList.add('active');
-    navSkills.classList.remove('active');
-    navContact.classList.remove('active');
+  navHome.classList.remove('active');
+  navAbout.classList.remove('active');
+  navWork.classList.add('active');
+  navSkills.classList.remove('active');
+  navContact.classList.remove('active');
 
-    sectionHome.style.display = 'none';
-    sectionAbout.style.display = 'none';
-    sectionWork.style.display = 'block';
-    sectionSkills.style.display = 'none';
-    sectionContact.style.display = 'none';
+  sectionHome.style.display = 'none';
+  sectionAbout.style.display = 'none';
+  sectionWork.style.display = 'block';
+  sectionSkills.style.display = 'none';
+  sectionContact.style.display = 'none';
 });
 
 navSkills.addEventListener('click', () => {
-    navHome.classList.remove('active');
-    navAbout.classList.remove('active');
-    navWork.classList.remove('active');
-    navSkills.classList.add('active');
-    navContact.classList.remove('active');
+  navHome.classList.remove('active');
+  navAbout.classList.remove('active');
+  navWork.classList.remove('active');
+  navSkills.classList.add('active');
+  navContact.classList.remove('active');
 
-    sectionHome.style.display = 'none';
-    sectionAbout.style.display = 'none';
-    sectionWork.style.display = 'none';
-    sectionSkills.style.display = 'block';
-    sectionContact.style.display = 'none';
+  sectionHome.style.display = 'none';
+  sectionAbout.style.display = 'none';
+  sectionWork.style.display = 'none';
+  sectionSkills.style.display = 'block';
+  sectionContact.style.display = 'none';
 });
 
 navContact.addEventListener('click', () => {
-    navHome.classList.remove('active');
-    navAbout.classList.remove('active');
-    navWork.classList.remove('active');
-    navSkills.classList.remove('active');
-    navContact.classList.add('active');
+  navHome.classList.remove('active');
+  navAbout.classList.remove('active');
+  navWork.classList.remove('active');
+  navSkills.classList.remove('active');
+  navContact.classList.add('active');
 
-    sectionHome.style.display = 'none';
-    sectionAbout.style.display = 'none';
-    sectionWork.style.display = 'none';
-    sectionSkills.style.display = 'none';
-    sectionContact.style.display = 'block';
+  sectionHome.style.display = 'none';
+  sectionAbout.style.display = 'none';
+  sectionWork.style.display = 'none';
+  sectionSkills.style.display = 'none';
+  sectionContact.style.display = 'block';
 });
